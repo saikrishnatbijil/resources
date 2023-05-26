12458df7a904c06faa95981c4aa23fe9dc201192
Saikrishna T Bijil <98866798+saikrishnatbijil@users.noreply.github.com>
2023-05-26 09:40:20 +0530

    Update makeAudio.py

--- makeAudio.py
+++ makeAudio.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 
 # text = str(input("Paste the script ::: "))
-text = "Have you ever wondered how much money mrbeast spent to make his studio. When Mrwhoosetheboss interviewed mrbeast, Mrwhoosetheboss counted the total value, and the total is 14 Million Dollars. Sub for more."
+text = "Have you ever wondered Sub for more."
 # text = "Exciting news just dropped! Google Pixel Fold is here Stay tech-savvy"
 
 tts = gTTS(text=text, lang='en', tld='us', slow=False)
